5a6c66d32965b22fc6d09339326da6e2afa5d7dd
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2021-03-29 16:40:33 +0530

    just a mistake

--- test.js
+++
@@ -1,4 +0,0 @@
-const search = require('./index');
-const { getDescription, parseUrl, PrepareUrl } = require('./utils.js');
-const url = 'https://www.youtube.com/watch%3Fv%3D8k-zyUyuvlM&sa=U&ved=2ahUKEwivl6j3qtXvAhWMVc0KHfyiD8MQtwIwDXoECAUQAQ&usg=AOvVaw24MiVlvcYClTo1CUNXaJvD'
-search('node.js', { safe: false }).then(r => console.log(r.searchResults))
