81efc3b98e515bb667ad0556fee5d626d8366168
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-08-22 12:11:37 +0530

    add website

---
+++ apps/website/theme.config.jsx
@@ -0,0 +1,21 @@
+export default {
+    logo: <img src="/transparent.png" height={70} width={70} />,
+    project: {
+      link: 'https://github.com/typicalninja/google-sr'
+    },
+    docsRepositoryBase: 'https://github.com/typicalninja/google-sr/tree/master/apps/website',
+    useNextSeoProps() {
+        return {
+          titleTemplate: '%s – GSR'
+        }
+    },
+    primaryHue: 174,
+    head: (
+        <>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta property="og:title" content="GSR" />
+          <meta property="og:description" content="The next site builder" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        </>
+      )
+}
