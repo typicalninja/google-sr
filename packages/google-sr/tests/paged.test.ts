cbe8012798263e886734d7141022ccbababecf1c
typicalninja <65993466+typicalninja@users.noreply.github.com>
2024-07-29 19:39:24 +0530

    Migrate tests from mocha to vitest (#16)
    * chore: vitest install & cleanup
    
    * chore: remove old tests
    
    * test(google-sr): Add new vitest powered tests
    
    * test(google-sr): Add paged api tests
    
    * test(google-sr): Use jest api instead of chai api
    
    * test(google-sr): fix tests

---
+++ packages/google-sr/tests/paged.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from "vitest";
+import {
+  searchWithPages,
+  ResultTypes,
+  OrganicResult,
+} from "../src";
+
+// since other search types are tested in result.test.ts, we only need to generic test paged results
+test("Search for paged search results", async () => {
+  const queryResult = await searchWithPages({
+    query: "nodejs",
+    resultTypes: [OrganicResult],
+    pages: 2,
+  });
+  expect(queryResult).toHaveLength(2)
+
+  // verify all results are OrganicResults
+  for (const page of queryResult) {
+    expect(page).length.greaterThan(0);
+    for (const result of page) {
+      expect(result.type).toBe(ResultTypes.OrganicResult);
+
+      // verify properties are present and not empty;
+      expect(result.link).toBeTypeOf("string")
+      expect(result.description).toBeTypeOf("string");
+      expect(result.title).toBeTypeOf("string");
+    }
+  }
+});
