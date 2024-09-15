8cf74c578ae441ba9e1cdc0df30617feeb241858
typicalninja <65993466+typicalninja@users.noreply.github.com>
2024-09-15 21:58:16 +0530

    docs: edit basic example description

--- apps/examples/src/basic.ts
+++ apps/examples/src/basic.ts
@@ -1,10 +1,27 @@
-/**
- * Basic usage of the library.
- * Uses the default result types. (OrganicResult)
- */
+import {
+	DictionaryResult,
+	// import the result types you want
+	OrganicResult,
+	// helpful to import ResultTypes to filter results
+	ResultTypes,
+	search,
+} from "google-sr";
 
-import { search } from "google-sr";
+const queryResult = await search({
+	query: "nodejs",
+	// OrganicResult is the default, however it is recommended to ALWAYS specify the result type
+	resultTypes: [OrganicResult, DictionaryResult],
+	// to add additional configuration to the request, use the requestConfig option
+	// which accepts a AxiosRequestConfig object
+	// OPTIONAL
+	requestConfig: {
+		params: {
+			// enable "safe mode"
+			safe: "active",
+		},
+	},
+});
 
-const results = await search({ query: "hello" });
-
-console.log(results);
+// will return a SearchResult[]
+console.log(queryResult);
+console.log(queryResult[0].type === ResultTypes.OrganicResult); // true
