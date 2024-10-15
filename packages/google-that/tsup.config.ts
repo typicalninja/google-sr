469aa4f22ee9804e2e9e6abd02c6ec3acb5be437
typicalninja <65993466+typicalninja@users.noreply.github.com>
2024-10-15 10:01:35 +0530

    chore: disallow dts builds for google-that cli

--- packages/google-that/tsup.config.ts
+++ packages/google-that/tsup.config.ts
@@ -1,7 +1,11 @@
-import { createTsupConfig } from "../../tsup.config";
+// due to type: module this needs to be imported as a .js file
+import { createTsupConfig } from "../../tsup.config.js";
 
 export default [
 	createTsupConfig({
 		format: ["esm"],
+		// cli only, no need for dts
+		dts: false,
+		minify: true,
 	}),
 ];
