1c712b0aa7f63610fd1e29e449f703a3b1734420
typicalninja <65993466+typicalninja@users.noreply.github.com>
2025-07-04 23:32:23 +0530

    refactor(google-sr): rename currency export to conversion

--- packages/google-sr/src/results/index.ts
+++ packages/google-sr/src/results/index.ts
@@ -2,6 +2,6 @@ export * from "./organic";
 export * from "./translate";
 export * from "./dictionary";
 export * from "./time";
-export * from "./currency";
+export * from "./conversion";
 export * from "./knowledge-panel";
 export * from "./news";
