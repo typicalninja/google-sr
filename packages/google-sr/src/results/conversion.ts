5c98f398554a50feb93c268aa98801d02562a31e
typicalninja <65993466+typicalninja@users.noreply.github.com>
2025-07-04 23:22:13 +0530

    feat(google-sr): move CurrencyResult to universal ConversionResult

--- packages/google-sr/src/results/conversion.ts
+++ packages/google-sr/src/results/conversion.ts
@@ -6,9 +6,9 @@ import {
 import { isEmpty, throwNoCheerioError } from "../utils";
 
 // Importing the Selectors from google-sr-selectors
-import { CurrencyConvertSelector, GeneralSelector } from "google-sr-selectors";
+import { ConversionSelector, GeneralSelector } from "google-sr-selectors";
 
-export interface CurrencyResultNode extends SearchResultNodeLike {
+export interface ConversionResultNode extends SearchResultNodeLike {
 	type: typeof ResultTypes.CurrencyResult;
 	from: string; // The currency being converted from
 	to: string; // The currency being converted to
@@ -18,18 +18,19 @@ export interface CurrencyResultNode extends SearchResultNodeLike {
  * Parses currency convert search results.
  * @returns Array of CurrencyResultNode
  */
-export const CurrencyResult: ResultSelector<CurrencyResultNode> = (
+export const ConversionResult: ResultSelector<ConversionResultNode> = (
 	$,
 	strictSelector,
 ) => {
 	if (!$) throwNoCheerioError("CurrencyResult");
 	const block = $(GeneralSelector.block).first();
 	const from = block
-		.find(CurrencyConvertSelector.from)
+		.find(ConversionSelector.from)
 		.text()
 		.replace("=", "")
 		.trim();
-	const to = block.find(CurrencyConvertSelector.to).text().trim();
+	const to = block.find(ConversionSelector.to).text().trim();
+
 	if (isEmpty(strictSelector, from, to)) return null;
 
 	return {
