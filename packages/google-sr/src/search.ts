3089ba5136a739bbcf9fbe24af50391a29a543d9
typicalninja <65993466+typicalninja@users.noreply.github.com>
2025-07-23 17:59:53 +0530

    refactor: use improved & cleaner types
    both search functions uses the new cleaner and simpler types

--- packages/google-sr/src/search.ts
+++ packages/google-sr/src/search.ts
@@ -12,40 +12,29 @@ import {
 	safeGetFetch,
 } from "./utils";
 
-/**
- * Search google with the given query, only 1 page is returned
- * @param options Search options
- * @returns Search results as an array of SearchResultNodes
- */
-export async function search<R extends ResultParser = typeof OrganicResult>(
-	options: SearchOptions<R, false>,
-): Promise<SearchResultTypeFromParser<R>[]>;
-export async function search<R extends ResultParser = typeof OrganicResult>(
-	options: SearchOptions<R, true>,
-): Promise<SearchResultTypeFromParser<R, true>[]>;
-export async function search<
-	R extends ResultParser = typeof OrganicResult,
-	N extends boolean = false,
->(options: SearchOptions<R, N>) {
-	if (!options)
+export async function search<R extends ResultParser, N extends boolean = false>(
+	searchOptions: SearchOptions<R, N>,
+): Promise<SearchResultTypeFromParser<R, N>[]> {
+	if (!searchOptions || typeof searchOptions !== "object") {
 		throw new TypeError(
-			`Search options must be provided. Received ${typeof options}`,
+			`Search options must be provided. Received ${typeof searchOptions}`,
 		);
+	}
 
-	const requestConfig = prepareRequestConfig(options);
+	const requestConfig = prepareRequestConfig(searchOptions);
 	const response = await safeGetFetch(requestConfig);
 	// we use the utility function to decode the data with the correct charset
 	const data = await decodeResponse(response);
 	const cheerioApi = load(data);
 	// use the provided parsers or the default one (OrganicResult)
-	const parsers = options.parsers || [OrganicResult];
-	let searchResults: SearchResultTypeFromParser<R>[] = [];
+	const parsers = searchOptions.parsers || [OrganicResult];
+	let searchResults: SearchResultTypeFromParser<R, N>[] = [];
 	// Iterate over each parser to call it with the cheerioApi and concatenate the results
 	for (const parser of parsers) {
 		const result = parser(
 			cheerioApi,
-			Boolean(options.noPartialResults),
-		) as SearchResultTypeFromParser<R>[];
+			Boolean(searchOptions.noPartialResults),
+		) as SearchResultTypeFromParser<R, N>;
 		// Result must be flattened to a single array
 		if (result) searchResults = searchResults.concat(result);
 	}
@@ -53,45 +42,6 @@ export async function search<
 	return searchResults;
 }
 
-/**
- * Searches google with the given query, returns results for multiple pages.
- * google uses cursor-based pagination (using param start=number).
- *
- * Therefore, when providing the specific page numbers, make sure to provide it in 10 increments.
- *
- * @example
- * ```ts
- * // search the first 5 pages
- * const results = await searchWithPages({
- *   query: "hello world",
- *   pages: 5,
- * });
- *
- * // or provide the specific page numbers
- * const results = await searchWithPages({
- *   query: "hello world",
- *   pages: [0, 10, 20, 30, 40],
- * });
- *
- * // pages can be skipped or be out of order
- * const results = await searchWithPages({
- *   query: "hello world",
- *   pages: [10, 0, 20],
- * });
- * ```
- * @returns Search results as an array of SearchResultNodes or an array of arrays of SearchResultNodes
- */
-// we have to handle overloads for both boolean and non-boolean noPartialResults
-export async function searchWithPages<
-	R extends ResultParser = typeof OrganicResult,
->(
-	options: SearchOptionsWithPages<R, false>,
-): Promise<SearchResultTypeFromParser<R>[][]>;
-export async function searchWithPages<
-	R extends ResultParser = typeof OrganicResult,
->(
-	options: SearchOptionsWithPages<R, true>,
-): Promise<SearchResultTypeFromParser<R, true>[][]>;
 export async function searchWithPages<
 	R extends ResultParser = typeof OrganicResult,
 	N extends boolean = false,
@@ -107,7 +57,7 @@ export async function searchWithPages<
 
 	// instead of using the above search() function,
 	// we must reimplement it in order to make it efficient, since it will call same function for each page unnecessarily
-	const searchResults: SearchResultTypeFromParser<R>[][] = [];
+	const searchResults: SearchResultTypeFromParser<R, N>[][] = [];
 	const pages = Array.isArray(options.pages)
 		? options.pages
 		: Array.from({ length: options.pages }, (_, i) => i * 10);
@@ -124,12 +74,12 @@ export async function searchWithPages<
 		// we use the utility function to decode the data with the correct charset
 		const data = await decodeResponse(response);
 		const cheerioApi = load(data);
-		let pageResults: SearchResultTypeFromParser<R>[] = [];
+		let pageResults: SearchResultTypeFromParser<R, N>[] = [];
 		for (const parser of parsers) {
 			const result = parser(
 				cheerioApi,
 				Boolean(options.noPartialResults),
-			) as SearchResultTypeFromParser<R>;
+			) as SearchResultTypeFromParser<R, N>;
 			// Result must be flattened to a single array
 			if (result) pageResults = pageResults.concat(result);
 		}
